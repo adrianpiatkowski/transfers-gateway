ce6b068f0624631b998c6ca874b9155b78fddd35
Adrian <adrian17091996@gmail.com>
2020-12-04 16:41:32 +0100

    first commit

---
+++ src/test/java/com/microservice/transfersgateway/TransfersGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.transfersgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfersGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
